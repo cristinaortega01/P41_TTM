f206b723881aef71222e438990e6d71e8a1df450
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-05-04 12:28:01 +0200

    List java

--- Front/src/main/java/com/example/front/HelloApplication.java
+++ Front/src/main/java/com/example/front/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.front;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +13,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        GridPane gridPane = new GridPane();
+        ChoiceBox levelselect = new ChoiceBox();
+        levelselect.getItems().addAll("Level 1", "Level 2", "Level 3");
+        ChoiceBox rangeselect = new ChoiceBox();
+        rangeselect.getItems().addAll("Bajo", "Contralto", "Tenor", "Bajo");
+        ToggleButton Select = new ToggleButton();
+        
+        gridPane.add(levelselect, 1, 1);
+        gridPane.add(rangeselect, 2,1);
+        Scene scene = new Scene(gridPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
