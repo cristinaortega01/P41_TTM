f7fd03a37924c3c338e2dd5cfdb4d3c88c451554
andreucastano01 <andreu.castano01@estudiant.upf.edu>
2022-05-11 11:16:29 +0200

    Title + help button

--- Front/src/main/java/com/example/front/HelloApplication.java
+++ Front/src/main/java/com/example/front/HelloApplication.java
@@ -2,28 +2,84 @@ package com.example.front;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private  HBox addHBox() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Text title = new Text("Music Tune Education");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        title.setTextAlignment(TextAlignment.CENTER);
+        hbox.getChildren().add(title);
+
+        StackPane stack = new StackPane();
+        Rectangle helpIcon = new Rectangle(30.0, 25.0);
+        helpIcon.setFill(new LinearGradient(0,0,0,1, true, CycleMethod.NO_CYCLE,
+                new Stop[]{
+                        new Stop(0, Color.web("#4977A3")),
+                        new Stop(0.5, Color.web("#B0C6DA")),
+                        new Stop(1,Color.web("#9CB6CF")),}));
+        helpIcon.setStroke(Color.web("#D0E6FA"));
+        helpIcon.setArcHeight(3.5);
+        helpIcon.setArcWidth(3.5);
+
+        Text helpText = new Text("?");
+        helpText.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+        helpText.setFill(Color.WHITE);
+        helpText.setStroke(Color.web("#7080A0"));
+
+        stack.getChildren().addAll(helpIcon, helpText);
+        stack.setAlignment(Pos.CENTER_RIGHT);     // Right-justify nodes in stack
+        StackPane.setMargin(helpText, new Insets(0, 10, 0, 0)); // Center "?"
+
+        hbox.getChildren().add(stack);            // Add to HBox from Example 1-2
+        HBox.setHgrow(stack, Priority.ALWAYS);    // Give stack any extra space
+
+        return hbox;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane gridPane = new GridPane();
+        BorderPane border = new BorderPane();
+        HBox hbox = addHBox();
+        border.setTop(hbox);
+
+
+        /*GridPane gridPane = new GridPane();
         ChoiceBox levelselect = new ChoiceBox();
         levelselect.getItems().addAll("Level 1", "Level 2", "Level 3");
         ChoiceBox rangeselect = new ChoiceBox();
         rangeselect.getItems().addAll("Bajo", "Contralto", "Tenor", "Bajo");
         ToggleButton Select = new ToggleButton();
-        
+
         gridPane.add(levelselect, 1, 1);
         gridPane.add(rangeselect, 2,1);
-        Scene scene = new Scene(gridPane, 320, 240);
-        stage.setTitle("Hello!");
+
+         */
+        Scene scene = new Scene(border, 960, 540);
+        stage.setTitle("Music Tune education!");
         stage.setScene(scene);
         stage.show();
     }
